6e3d552c23815bea08a3723610197937095ab605
pgoygeordi <pgoygeordi@outlook.com>
2017-03-07 17:11:31 +0800

    fixed type list page title order by function and moved pagination into individual function.

--- application/controllers/Lists.php
+++ application/controllers/Lists.php
@@ -2,6 +2,25 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Lists extends MY_Controller {
     private $perPage = 30;
+    private function doPagination($totalRows){
+        //http://codeigniter.org.cn/user_guide/libraries/pagination.html#id7
+        //1、载入分页类    
+        $this->load->library('pagination');
+
+        //2、配置项设置
+        $config['base_url'] = site_url('/lists/view/type');
+        $config['total_rows'] = $totalRows;
+        $config['per_page'] = $this->perPage;
+        $config['uri_segment'] = 4;
+        $config['first_link'] = '第一页';
+        $config['prev_link'] = '上一页';
+        $config['next_link'] = '下一页';
+        $config['last_link'] = '最后一页';
+        //$config['use_page_numbers'] = TRUE;
+
+        //3、使用配置初始化分页类
+        $this->pagination->initialize($config);
+    }
     public function view($page = 'type', $offset=0)
     {
         //每页显示3条信息
@@ -29,36 +48,11 @@ class Lists extends MY_Controller {
                 $query = $this->part_model->select_entry(array('pageOffset'=>$offset, 'rowsPerPage'=>$this->perPage));
                 break;
         }
-        //http://codeigniter.org.cn/user_guide/libraries/pagination.html#id7
-        //1、载入分页类    
-        $this->load->library('pagination');
-
-        //2、配置项设置
-        $config['base_url'] = site_url('/lists/view/type');
-        $config['total_rows'] = $totalRows;
-        $config['per_page'] = $this->perPage;
-        $config['uri_segment'] = 4;
-        $config['first_link'] = '第一页';
-        $config['prev_link'] = '上一页';
-        $config['next_link'] = '下一页';
-        $config['last_link'] = '最后一页';
-        //$config['use_page_numbers'] = TRUE;
-
-        //3、使用配置初始化分页类
-        $this->pagination->initialize($config);
-        /*
-        $b['links'] = $this->pagination->create_links();
-        $offset = $this->uri->segment(4);
-        $this->db->limit($perPage, $offset);*/
-
-        /*
-        $b['goods'] = $this->g->select('goods'); 
-        $this->load->view('admin/goodslist.html',$b);*/
-        //die("orderBy = $orderBy, table = $table, option = $searchOpt");
+        $this->doPagination($totalRows);
         $data["listData"] = $query;
         $this->goToView($page, $data);
     }
-    public function doSearch(){
+    public function doSearch($offset=0){
         //$data['title'] = ucfirst($page); // Uppercase the first letter
         $data['siteTitle'] = '北京欣晶华汽车修理厂';
         $data['authorCopy'] = '<a href="mailto:pgoygeordi@outlook.com">PGOY</a> 2016';
@@ -81,7 +75,6 @@ class Lists extends MY_Controller {
             case "part":
                 break;
             //default:
-
         }
         if(!!$table){
             $table = "type";
@@ -90,13 +83,17 @@ class Lists extends MY_Controller {
             'table' => $table, 
             'orderBy' => $orderBy,
             'orderDir' => $orderDir,
+            'pageOffset'=>$offset, 
+            'rowsPerPage'=>$this->perPage,
         ];        
         if(!!$searchOpt){
             $options["where"] = "where `name` like '%$searchOpt%' or `fuelType` like '%$searchOpt%' or `capacity` like '%$searchOpt%'";
         }
-        
-        $query = $this->conditionalQuery($options);
-        $data["typeList"] = $query;
+        $queryAll = $this->type_model->select_entry();
+        $totalRows = $queryAll->num_rows();
+        $query = $this->type_model->select_entry($options);
+        $this->doPagination($totalRows);
+        $data["listData"] = $query;
         $this->goToView($table, $data);
     }
     private function getOrderByColumnForType($val){
