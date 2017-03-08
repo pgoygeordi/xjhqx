d03fa75c939a3036c86c8139ab42371db6317770
pgoygeordi <pgoygeordi@outlook.com>
2017-03-08 13:00:06 +0800

    reorder left menu item.

--- application/views/siders/left.php
+++ application/views/siders/left.php
@@ -1,10 +1,11 @@
 <div id="leftNav">
     <div id="leftNavBar">
+        <div><strong>菜单：</strong></div>
         <ul>
-            <li>客户管理</li>
             <li><a href="<?php echo site_url('lists/view/history');?>" target="_self">修理记录管理</a></li>
             <li><a href="<?php echo site_url('lists/view/type');?>" target="_self">车辆类型管理</a></li>
             <li><a href="<?php echo site_url('lists/view/part');?>" target="_self">车辆配件管理</a></li>
+            <li>客户管理</li>
         </ul>
     </div>
     <div id="leftNavButton"></div>
